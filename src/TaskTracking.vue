1e58236932dc44fab1e87358b63943f7019b717d
Serey002 <serey.phem.12022006@gmail.com>
2026-06-04 15:23:00 +0700

    Extend Task Manager: add priority (v-model select: Low / Medium / High)

---
+++ src/TaskTracking.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mb-6">
+        <div class="text-3xl font-bold text-slate-800">Task Tracking</div>
+    </div>
+    <div>
+        <select v-model="newTaskPriority" name="" id="">
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+        </select>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    const newTaskText = ref('');
+    const newTaskPriority = ref('Low')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
